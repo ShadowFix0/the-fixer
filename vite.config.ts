e2d2709dae9871e77a6e0b684fab676199c98df0
acer <ttt146159@gmail.com>
2026-05-07 23:27:08 +0300

    fix: dynamic base path for github pages support

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/',
+    base: process.env.GITHUB_ACTIONS ? '/the-fixer/' : '/',
     plugins: [
       react(), 
       tailwindcss(),
